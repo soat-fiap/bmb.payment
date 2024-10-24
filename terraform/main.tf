e2b28189b64c7c239bf81136aa06c803ad6ce96e
italo <italo.pessoa@hotmail.com>
2024-10-24 07:17:59 -0300

    chore: rename tfvars file folder fix variables

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,9 +2,9 @@
 # EKS CLUSTER
 ##############################
 
-# data "aws_eks_cluster" "techchallenge_cluster" {
-#   name = var.eks_cluster_name
-# }
+data "aws_eks_cluster" "techchallenge_cluster" {
+  name = var.eks_cluster_name
+}
 
 ##############################
 # DATABASE
