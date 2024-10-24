bac1650f278e0173bf31159ec5c7e6a8d0f8fcdd
italo <italo.pessoa@hotmail.com>
2024-10-24 06:58:08 -0300

    chore: add masstransit healthcheck #22

--- src/Bmb.Payment.Bus/ServiceCollectionsExtensions.cs
+++ src/Bmb.Payment.Bus/ServiceCollectionsExtensions.cs
@@ -1,8 +1,8 @@
 ﻿using System.Diagnostics.CodeAnalysis;
 using Bmb.Payment.Core.Contracts;
 using MassTransit;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
 
 namespace Bmb.Payment.Bus;
 
@@ -11,16 +11,23 @@ public static class ServiceCollectionsExtensions
 {
     public static void AddPaymentBus(this IServiceCollection services)
     {
-        services.AddMassTransit(x =>
+        services.AddMassTransit(bus =>
         {
-            x.AddConsumer<OrderCreatedConsumer>();
-            x.UsingAmazonSqs((context, cfg) =>
+            bus.AddConsumer<OrderCreatedConsumer>();
+            bus.UsingAmazonSqs((context, cfg) =>
             {
                 cfg.Host("us-east-1", h =>
                 {
                     // h.Scope("dev", true);
                 });
                 cfg.ConfigureEndpoints(context, new KebabCaseEndpointNameFormatter( false));
+            });
+            
+            bus.ConfigureHealthCheckOptions(options =>
+            {
+                options.Name = "masstransit";
+                options.MinimalFailureStatus = HealthStatus.Unhealthy;
+                options.Tags.Add("health");
             });
         });
 
